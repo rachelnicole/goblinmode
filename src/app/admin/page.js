1c44d1f8265f1cc59f8737d58b91f196fc5f955f
rachelnicole <loveless@gmail.com>
2023-05-01 16:33:25 -0400

    basic styling

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -2,15 +2,22 @@
 import React from "react";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
+import Header from './Header'
+
 function Page() {
     const { user } = useAuthContext()
     const router = useRouter()
+
+    console.log(user.email);
 
     React.useEffect(() => {
         if (user == null) router.push("/")
     }, [user])
 
-    return (<h1>Only logged in users can view this page</h1>);
+    return (<div>
+    <Header/>
+    <h1>Only logged in users can view this page.  {user.email}</h1>
+    </div>);
 }
 
 export default Page;
